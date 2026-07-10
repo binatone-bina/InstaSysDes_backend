3d2313859c8ac1b345d2d1e9502b7ca9d4852d57
binatone-bina <swabist0077@gmail.com>
2026-07-10 18:49:17 +0530

    avatar.jpg

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Adjust this to your frontend URL
+  origin: process.env.FRONTEND_URL || 'https://connectSphere.vercel.app', 
   credentials: true, // Allow cookies to be sent
 }));
 
